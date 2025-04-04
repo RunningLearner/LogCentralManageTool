c7b1126fd72eb8a33e41d81daec4fd15451282eb
sinam <sinam@medicalstandard.com>
2025-04-04 11:56:04 +0900

    feat: MainView 생성

--- LogCentralManageTool/Views/MainWindow.xaml.cs
+++ LogCentralManageTool/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LogCentralManageTool;
+namespace LogCentralManageTool.Views;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
