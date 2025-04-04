33e436ca65f4b6df3c0ea9435dce23e7e97853f2
sinam <sinam@medicalstandard.com>
2025-04-04 16:13:48 +0900

    refactor: 사이드바 분리

---
+++ LogCentralManageTool/Utils/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Input;
+
+namespace LogCentralManageTool.Utils
+{
+    /// <summary>
+    /// MVVM에서 ICommand 구현을 위한 RelayCommand 클래스
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        /// <summary>
+        /// RelayCommand 생성자
+        /// </summary>
+        /// <param name="execute">실행할 액션</param>
+        /// <param name="canExecute">실행 가능 여부를 판단하는 함수 (옵션)</param>
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// ICommand.CanExecuteChanged 이벤트
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        /// <summary>
+        /// 명령 실행 가능 여부 확인
+        /// </summary>
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+
+        /// <summary>
+        /// 명령 실행
+        /// </summary>
+        public void Execute(object parameter) => _execute(parameter);
+    }
+}
---
+++ LogCentralManageTool/ViewModels/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using LogCentralManageTool.Utils;
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LogCentralManageTool.ViewModels
+{
+    /// <summary>
+    /// 메인 윈도우의 ViewModel: 사이드바 토글 기능 포함
+    /// </summary>
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _isSidebarVisible = true;
+
+        /// <summary>
+        /// 사이드바의 표시 여부 (true: 보임, false: 숨김)
+        /// </summary>
+        public bool IsSidebarVisible
+        {
+            get => _isSidebarVisible;
+            set
+            {
+                if (_isSidebarVisible != value)
+                {
+                    _isSidebarVisible = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(SidebarWidth));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 사이드바 열의 너비: 보이면 200, 숨기면 0
+        /// </summary>
+        public GridLength SidebarWidth => IsSidebarVisible ? new GridLength(200) : new GridLength(0);
+
+        private ICommand _toggleSidebarCommand;
+
+        /// <summary>
+        /// 사이드바를 토글하는 명령
+        /// </summary>
+        public ICommand ToggleSidebarCommand => _toggleSidebarCommand ??= new RelayCommand(o => ToggleSidebar());
+
+        /// <summary>
+        /// 사이드바 표시 상태를 토글합니다.
+        /// </summary>
+        private void ToggleSidebar()
+        {
+            IsSidebarVisible = !IsSidebarVisible;
+        }
+
+        /// <summary>
+        /// 속성 변경 알림 메서드
+        /// </summary>
+        /// <param name="propertyName">변경된 속성명</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
